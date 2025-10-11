438dcef419de5de01048469a3cf4c7e3400b0a6a
Shivansh4497 <69387970+Shivansh4497@users.noreply.github.com>
2025-10-12 00:29:48 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        indigoDeep: '#0f1724',
+        tealAccent: '#2dd4bf'
+      }
+    }
+  },
+  plugins: [],
+}
